5ec7883a26650fc9772a0328afa0d4e9365fa27d
Smit Talashilkar <smittalashilkar@gmail.com>
2021-07-29 19:23:13 +0530

    first ui library setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+import React from "react";
+
+export const ButtonComponent = () => {
+  //   React.useEffect(() => {
+  //     console.log("Use effect called");
+  //   }, []);
+  return <Button type="primary">Primary Button</Button>;
+};
+
+console.log("bundled");
