53e320b2dd9732ed3a6190076248ab75c38ee803
Smit Talashilkar <smittalashilkar@gmail.com>
2021-07-29 19:47:07 +0530

    package and rollup updated

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,31 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
 import babel from "@rollup/plugin-babel";
+import pkg from "./package.json";
+const extensions = [".js", ".ts"];
+
 export default {
   input: "index.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "es",
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+  ],
   plugins: [
-    babel({
-      presets: ["@babel/preset-react"],
-    }),
+    babel(
+      {
+        presets: ["@babel/preset-react"],
+      },
+      {
+        exclude: "node_modules/**",
+        extensions,
+      }
+    ),
     peerDepsExternal(),
     resolve(),
     commonjs(),
